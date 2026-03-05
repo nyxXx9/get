91a327f030201b374b00abfc9a3af4f26ac26b70
Дарья Ускова <RomanovnDa@yandex.com>
2026-03-05 18:13:38 +0300

    Update r2r-sar.py

--- get-adc/r2r-sar.py
+++ get-adc/r2r-sar.py
@@ -1,1 +1,22 @@
+import r2r_adc as adc
+import adc_plot as plot
+import time
 
+ADC = adc.R2R_ADC(3.3)
+voltages = []
+times = []
+duration = 3.0
+
+try:
+    start_time = time.time()
+    while (time.time() - start_time < duration):
+        value = ADC.get_sar_voltage()
+        current_time = time.time()-start_time
+
+        voltages.append(value)
+        times.append(current_time)
+        
+    plot.plot_voltage_vs_time(times, voltages, ADC.dynamic_range*1.1)
+    plot.plot_sampling_period_hist(times)
+finally:
+    ADC.deinit()
