0bbe63a6e138c24cd223d11e7b2ec5bd60c3984c
Дарья Ускова <RomanovnDa@yandex.com>
2026-03-12 18:20:36 +0300

    Update signal_generator.py

--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -1,11 +1,11 @@
+import numpy as np
 import time
 
-def get_sin_wave_amplitude(freq, time):
-    from math import sin, pi
-    raw = sin(2.0 * pi * freq * time)
+def get_sin_wave_amplitude(freq, t):
+    raw = (np.sin(2 * np.pi * freq * t) + 1)/2
 
     return raw
 
 def wait_for_sampling_period(sampling_frequency):
-    period = 1.0 / sampling_frequency
+    period = 1 / sampling_frequency
     time.sleep(period)
