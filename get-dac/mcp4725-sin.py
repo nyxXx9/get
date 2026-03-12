92721a8a801b5594128a28967d2d5fd596f80152
Дарья Ускова <RomanovnDa@yandex.com>
2026-03-12 19:57:29 +0300

    Update MCP4725 initialization and amplitude value

--- get-dac/mcp4725-sin.py
+++ get-dac/mcp4725-sin.py
@@ -1,22 +1,20 @@
-import mcp4725_driver as mcp4725
+import mcp4725_driver as mcp
 import signal_generator as sg
-
-
 import time
 
-amplitude = 1.5
+amplitude = 2.2
 signal_frequency = 10
 sampling_frequency = 1000
 
 if __name__ == "__main__":
     try:
-        dac = mcp4725.MCP4725(2.6, 0x61 , False)
+        dac = mcp.MCP4725(5.0, 0x61 , False)
         start_time = time.time()
 
         while True:
-            t = time.time() - start_time
+            current_time = time.time() - start_time
 
-            norm_amp = sg.get_sin_wave_amplitude(signal_frequency, t)
+            norm_amp = sg.get_sin_wave_amplitude(signal_frequency, current_time)
 
             voltage = amplitude * norm_amp
 
