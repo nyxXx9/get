a5324a8e29622fc95e99bf04ad24eee5c8f966a9
Дарья Ускова <RomanovnDa@yandex.com>
2026-03-12 19:56:07 +0300

    Refactor pwm_dac import and update parameters

--- get-dac/pwm-sin.py
+++ get-dac/pwm-sin.py
@@ -1,21 +1,20 @@
-import pwm_dac
+import pwm_dac as pwm
 import signal_generator as sg
-
 import time
 
-amplitude = 1.5
+amplitude = 3.2
 signal_frequency = 10
-sampling_frequency = 500
+sampling_frequency = 1000
 
 if __name__ == "__main__":
     try:
-        dac = pwm_dac.PWM_DAC(12, 500, 3.290, False)
+        dac = pwm.PWM_DAC(12, 500, 3.290, False)
         start_time = time.time()
 
         while True:
-            t = time.time() - start_time
+            current_time = time.time() - start_time
 
-            norm_amp = sg.get_sin_wave_amplitude(signal_frequency, t)
+            norm_amp = sg.get_sin_wave_amplitude(signal_frequency, current_time)
 
             voltage = amplitude * norm_amp
 
