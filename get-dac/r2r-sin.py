f7826d0a97cd7a777364cd833d4a058a9873e949
Дарья Ускова <RomanovnDa@yandex.com>
2026-03-12 18:22:18 +0300

    Update r2r-sin.py

--- get-dac/r2r-sin.py
+++ get-dac/r2r-sin.py
@@ -2,26 +2,24 @@ import r2r_dac as r2r
 import signal_generator as sg
 import time
 
-amplitude = 3.0
+amplitude = 3.2
 signal_frequency = 10
 sampling_frequency = 1000
 
 if __name__ == "__main__":
     try:
         dac = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.3)
-
-        t = 0.0
-        dt = 1 / sampling_frequency
+        start_time = time.time()
+        
         while True:
-
-            norm_amp = sg.get_sin_wave_amplitude(signal_frequency, t)
+            current_time = time.time() - start_time
+            norm_amp = sg.get_sin_wave_amplitude(signal_frequency, current_time)
 
             voltage = amplitude * norm_amp
 
             dac.set_voltage(voltage)
 
             sg.wait_for_sampling_period(sampling_frequency)
-            t += dt
 
     finally:
         dac.deinit()
